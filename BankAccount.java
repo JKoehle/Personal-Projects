2c0327e2c011bd43facf97733bfb275760b9faf7
JKoehle <118227709+JKoehle@users.noreply.github.com>
2022-11-14 16:39:13 -0700

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,43 +1,47 @@
-package simpleBank;
-
-public class BankAccount {
-	public static void main(String[] args) {
-	    
-	  }
-	String accountHolderName;
+public class BankAccount
+{
+    String accountHolderName;
     String accountNumber;
     double accountBalance;
- 
     void deposit(double amount) 
     {
         accountBalance += amount;
         System.out.printf("Receipt { $%.2f deposited. New balance is $%.2f }\n", amount, accountBalance);
     }
-    
     void withdraw (double amount) 
     {
        accountBalance -= amount;
        System.out.printf("Receipt { $%.2f withdrawn. New balance is $%.2f }\n", amount, accountBalance);
     }
-	   public static BankAccount makeBankAccount (String accname, String accnumber) {
+	   public static BankAccount makeBankAccount (String accname, String accnumber) 
+	   {
 	      BankAccount account = new BankAccount();
 	      account.accountHolderName = accname;
 	      account.accountNumber = accnumber; 
 	      account.accountBalance =0.0;
-	      return account; }
-	   public static void displayAccountInfo (BankAccount account) {
+	      return account;
+	   }
+	   public static void displayAccountInfo (BankAccount account) 
+	   {
 	      System.out.println("Account number      : " + account.accountNumber);
 	      System.out.println("Account holder name : " + account.accountHolderName);
-	      System.out.printf("Account Balance     : $%.2f\n", account.accountBalance); }
-	   public static void chargeServiceFee (BankAccount account, double fee) {
-	      account.accountBalance -= fee; }
-	   public static void applyInterest (BankAccount account, double rate) {
+	      System.out.printf("Account Balance     : $%.2f\n", account.accountBalance); 
+	   }
+	   public static void chargeServiceFee (BankAccount account, double fee) 
+	   {
+	      account.accountBalance -= fee; 
+	   }
+	   public static void applyInterest (BankAccount account, double rate)
+	   {
 	      double interest = account.accountBalance * rate;
-	      account.accountBalance += interest; }
-	   public static void payBill (BankAccount account, double bill) {
+	      account.accountBalance += interest;
+	   }
+	   public static void payBill (BankAccount account, double bill) 
+	   {
 	      account.withdraw(bill);
 	      }
-	   public static void depositChecks (BankAccount account, double pay) {
+	   public static void depositChecks (BankAccount account, double pay)
+	   {
 	      account.deposit(pay);
 	      }
 }
